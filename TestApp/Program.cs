3b1194d8b78823320ecab8b28c1cba63acb7f999
Dmitry.Kazantsev <dmitry.kazantsev@jetbrains.com>
2026-09-15 14:07:00 +0200

    Reproduced

---
+++ TestApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Source Generator запущен успешно!");
+// Класс GeneratedHeavyData создан генератором. Мы можем его увидеть через рефлексию
+var type = Type.GetType("GeneratedNamespace.GeneratedHeavyData");
+Console.WriteLine($"Сгенерированный класс найден: {type != null}");
