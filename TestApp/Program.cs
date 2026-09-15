91444131e0aa0aa23934459f66031a98948fb11e
Dmitry.Kazantsev <dmitry.kazantsev@jetbrains.com>
2026-09-15 16:02:00 +0200

    Optimization showcase

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Reflection;
 
-Console.WriteLine("Source Generator запущен успешно!");
+Console.WriteLine("Source Generator launched");
 
-// Способ 1: Ищем класс в конкретной сборке консольного приложения
 var currentAssembly = Assembly.GetExecutingAssembly();
 var typeViaAssembly = currentAssembly.GetType("GeneratedNamespace.GeneratedHeavyData");
-Console.WriteLine($"Сгенерированный класс найден (через Assembly): {typeViaAssembly != null}");
+Console.WriteLine($" GeneratedNamespace.GeneratedHeavyData: {typeViaAssembly != null}");
 
-// Способ 2: Или можно просто использовать класс напрямую в коде без всякой рефлексии!
-// Компилятор его уже видит. Можем проверить длину массива:
 int arrayLength = GeneratedNamespace.GeneratedHeavyData.Data.Length;
-Console.WriteLine($"Длина восстановленного массива данных: {arrayLength:N0} байт");
+Console.WriteLine($"Restored array length: {arrayLength:N0} bytes");
